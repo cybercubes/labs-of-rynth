36217d08dad041a694d056df279a51b943dc308a
Darova <fords1234@mail.ru>
2019-02-09 17:06:44 +0200

    я созадл тестового моба

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,7 +1,8 @@
-package;
+package ;
 
 import flixel.FlxState;
 import actors.Player;
+import actors.Monster;
 import actors.Coin;
 import flixel.tile.FlxTilemap;
 import flixel.addons.editors.ogmo.FlxOgmoLoader;
@@ -11,6 +12,7 @@ import flixel.group.FlxGroup;
 
 class PlayState extends FlxState
 {
+	var _monsterS:FlxTypedGroup<Monster>;
 	var _player:Player;
 	var _map:FlxOgmoLoader;
 	var _mWalls:FlxTilemap;
@@ -20,7 +22,7 @@ class PlayState extends FlxState
 	{
 		super.create();
 		
-		_map = new FlxOgmoLoader(AssetPaths.room001__oel);
+		_map = new FlxOgmoLoader(AssetPaths.room002__oel);
 
 		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
 		_mWalls.follow();
@@ -28,7 +30,9 @@ class PlayState extends FlxState
 		_mWalls.setTileProperties(2, FlxObject.ANY);
 
 		_grpCoins = new FlxTypedGroup<Coin>();
-		
+
+		_monsterS = new FlxTypedGroup<Monster>();
+
 		_player = new Player();
 		
 		_map.loadEntities(placeEntities, "entities");
@@ -36,6 +40,7 @@ class PlayState extends FlxState
 		add(_mWalls);
 		add(_grpCoins);
 		add(_player);
+		add(_monsterS);
 
 		FlxG.camera.follow(_player, TOPDOWN, 1);
 	}
@@ -46,6 +51,18 @@ class PlayState extends FlxState
 
 		FlxG.collide(_player, _mWalls);
 		FlxG.overlap(_player, _grpCoins, playerTouchCoin);
+		FlxG.collide(_monsterS, _mWalls);
+ 		_monsterS.forEachAlive(checkEnemyVision);
+	}
+	 function checkEnemyVision(e:Monster):Void
+	{
+		if (_mWalls.ray(e.getMidpoint(), _player.getMidpoint()))
+		{
+			e.seesPlayer = true;
+			e.playerPos.copyFrom(_player.getMidpoint());
+		}
+		else
+			e.seesPlayer = false;
 	}
 
 	function placeEntities(entityName:String, entityData:Xml):Void
@@ -60,6 +77,10 @@ class PlayState extends FlxState
 		else if (entityName == "coin")
 		{
 			_grpCoins.add(new Coin(x + 4, y + 4));
+		}
+		else if (entityName == "monster")
+		{
+			 _monsterS.add(new Monster(x + 4, y, Std.parseInt(entityData.get("etype"))));
 		}
 	}
 
