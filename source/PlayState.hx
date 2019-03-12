51f6164b43c806fd6cb94527bf5bbea584272a5b
cybercubes <cyberandcubes@gmail.com>
2019-03-12 11:17:27 +0200

    removed unnecessary imports and classes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,7 @@
 package ;
 
 import flixel.util.FlxColor;
-import flixel.addons.weapon.FlxWeapon;
 import flixel.FlxState;
-import actors.Player;
 import actors.brain.Monster;
 import flixel.tile.FlxTilemap;
 import flixel.addons.editors.ogmo.FlxOgmoLoader;
@@ -14,7 +12,6 @@ import actors.Player;
 import item.BaseItem;
 import item.passive.PassiveItem;
 import item.active.ConsumableItem;
-import flixel.addons.weapon.FlxBullet;
 import flixel.util.helpers.FlxBounds;
 import flixel.math.FlxPoint;
 
@@ -95,16 +92,6 @@ class PlayState extends FlxState
 					_grpItems.add(new ConsumableItem(x, y, name, 20));
 				case "diamond":
 					_grpItems.add(new PassiveItem(x, y, name));
-				case "pistol":
-					_grpItems.add(new FlxWeapon(name, // the following function is the factory function.
-						// FlxWeapon will call this function to get an instance of
-						// bullet whenever it has to create one
-					function(weapon) {
-						// so you create the bullet, do something to it, then return it
-						var b = new FlxBullet();
-						b.makeGraphic(4, 4, FlxColor.RED);
-						return b;
-					}, FlxWeaponFireFrom.PARENT(_player, new FlxBounds<FlxPoint>(FlxPoint.get(0, 0))), FlxWeaponSpeedMode.SPEED(new FlxBounds<Float>(500)), x, y));
 			}
 		}
 		else if (entityName == "monster")
